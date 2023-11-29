a8a0f4c99b7d427b7f47180a532e6cf0b244a2e5
Patiphan Orbesen <porbesen09@gmail.com>
2023-11-28 23:19:12 -0700

    Added event listener for button

--- js/app.js
+++ js/app.js
@@ -8,4 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Get user's location with submit button 
     const submitButton = document.getElementById('submitLocation');
+
+    // Add EventListener to the "Enter" button
+    submitButton.addEventListener('click', async () => {
+        
+        // Get value entered from the input box
+        const locationInput = document.getElementById('locationInput').value;
+
+        // Check if the there any value entered in the box
+        if (locationInput) {
+            // fetch current weather data for the input location
+            const weatherResponse = await fetch('${weatherEndpoint}?q=${locationInput}&appid=${apiKey}&units=metric');
+            const weatherData = await weatherResponse.json();
+        }
+    })
 })
