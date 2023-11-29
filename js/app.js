3840ccda7c799755b2b862ebd94bf961e96a4999
Patiphan Orbesen <porbesen09@gmail.com>
2023-11-28 23:02:49 -0700

    Setup API Endpoint in .js and added location input box

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Add event listener 
+// Set up API and endpoints
+
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = 'USER_API_KEY'; // User need to put their own api here 
+    const weatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather';
+    const forecastEndpoint = 'https://api.openweathermap.org/data/2.5/forecast';
+
+    // Get user's location with submit button 
+    const submitButton = document.getElementById('submitLocation');
+})
